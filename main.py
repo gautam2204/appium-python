eb38cc6cfc8817c572e80d92aa82f57e775a3aba
Gautam Rawat <Gautam.rawat123@gmail.com>
2023-09-08 07:13:20 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+from appium import webdriver
+
+caps =  dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    browserName='Chrome'
+)
+
+driver = webdriver.Remote("http://127.0.0.1:4723",desired_capabilities=caps)
+driver.get("https://google.com")
+print(driver.title)
+time.sleep(3)
+driver.quit()
---
+++ readme.md
@@ -0,0 +1,9 @@
+Setup:-
+<p>
+1. On Phone :-
+
+    - Switch to developers mode
+    - Switch Mobile to mobile threatering 
+    - In developers options turn ON USB Debugging
+    - Chines Phone (Disable Permission monitoring) 
+</p>
