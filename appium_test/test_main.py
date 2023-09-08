84aa6cf15d8e2b60b49cdb594b9ecd80da198306
Gautam Rawat <Gautam.rawat123@gmail.com>
2023-09-08 07:32:38 +0530

    basic setup

---
+++ appium_test/test_main.py
@@ -0,0 +1,17 @@
+import time
+
+from appium import webdriver
+
+def test_launchGoogleChrome():
+    caps =  dict(
+        platformName='Android',
+        automationName='uiautomator2',
+        deviceName='Android',
+        browserName='Chrome'
+    )
+
+    driver = webdriver.Remote("http://127.0.0.1:4723",desired_capabilities=caps)
+    driver.get("https://google.com")
+    print(driver.title)
+    time.sleep(3)
+    driver.quit()
